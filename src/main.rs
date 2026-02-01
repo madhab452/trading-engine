330861111419bf41030c760a7b333463872c04e7
Madhab Acharya <madhab452@gmail.com>
2026-02-01 21:43:12 +0700

    feat: #1

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+enum BidOrAsk {
+    Bid,
+    Ask,
+}
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
+struct Price {
+    integral: u64,
+    fractional: u64,
+    scalar: u64,
+}
+
+impl Price {
+    fn new(price: f64) -> Price {
+        let scalar: u64 = 100000;
+        let integral = price as u64;
+        let fractional = ((price % 1.0) * scalar as f64) as u64;
+        Price {
+            integral,
+            fractional,
+            scalar,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Order {
+    bid_or_ask: BidOrAsk,
+    size: f64,
+}
+
+impl Order {
+    fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
+        return Order { bid_or_ask, size };
+    }
+}
+
+#[derive(Debug)]
+struct Limit {
+    price: Price,
+    orders: Vec<Order>,
+}
+
+impl Limit {
+    fn new(price: Price) -> Limit {
+        Limit {
+            price,
+            orders: Vec::new(),
+        }
+    }
+
+    fn add_order(&mut self, order: Order) {
+        self.orders.push(order);
+    }
+}
+
+#[derive(Debug)]
+struct Orderbook {
+    asks: HashMap<Price, Limit>,
+    bids: HashMap<Price, Limit>,
+}
+
+impl Orderbook {
+    fn new() -> Orderbook {
+        Orderbook {
+            asks: HashMap::new(),
+            bids: HashMap::new(),
+        }
+    }
+
+    fn add_order(&mut self, price: f64, order: Order) {
+        match order.bid_or_ask {
+            BidOrAsk::Bid => {
+                let price = Price::new(price);
+                match self.bids.get_mut(&price) {
+                    Some(limit) => {
+                        limit.add_order(order);
+                    }
+                    None => {
+                        let mut limit = Limit::new(price);
+                        limit.add_order(order);
+                        self.bids.insert(price, limit);
+                    }
+                }
+            }
+            BidOrAsk::Ask => {}
+        }
+    }
+}
+
+fn main() {
+    let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
+    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 2.45);
+    let mut orderbook = Orderbook::new();
+    orderbook.add_order(4.4, buy_order_from_bob);
+    orderbook.add_order(4.4, buy_order_from_alice);
+    println!("{:?}", orderbook)
+}
